491a31445ee3fff214ee833651dbb3cdac1174e6
malucius-rh <malucius@redhat.com>
2025-10-22 08:34:36 -0400

    adding no_packages option

--- iozone/iozone_run.sh
+++ iozone/iozone_run.sh
@@ -314,7 +314,7 @@ fi
 source test_tools/general_setup "$@"
 
 # Install needed packages based on what's listed in the wrapper's json file
-${TOOLS_BIN}/package_tool --wrapper_config ${run_dir}/iozone-wrapper.json
+${TOOLS_BIN}/package_tool --no_packages $to_no_pkg_install --wrapper_config ${run_dir}/iozone-wrapper.json
 if [[ $? -ne 0 ]]; then
         exit_out "package_tool reported failure installing dependencies." 1
 fi
