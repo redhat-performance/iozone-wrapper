61546adbbbc3fb892c1ef6597a6bb0f6a198cc68
malucius-rh <malucius@redhat.com>
2026-03-17 13:48:09 -0400

    Update how we get test tools and make use of $TOOLS_BIN

--- iozone/iozone_run.sh
+++ iozone/iozone_run.sh
@@ -58,7 +58,7 @@ iozone_version="v1.0"
 
 
 # Gather hardware information
-${curdir}/test_tools/gather_data ${curdir}
+${TOOLS_BIN}/gather_data ${curdir}
 
 if [ ! -f "/tmp/${test_name}.out" ]; then
         command="${0} $@"
@@ -168,6 +168,9 @@ modes2run=0;
 args_fs_types=""
 
 tools_git=https://github.com/redhat-performance/test_tools-wrappers
+TOOLS_BIN="$HOME/test_tools"
+export TOOLS_BIN
+
 #
 # Config info
 #
@@ -258,7 +261,7 @@ usage()
 	echo "   --devices_to_use /dev/nvme3n1,/dev/nvme2n1 --filesystems xfs --file_count_list 1,2"
 	echo "   --test_prefix io_test_all --max_file_size 64"
 	echo "   --test_type 0,1,2,3,4,5,6,7,8,9,10,11,12"
-	source test_tools/general_setup --usage
+	source ${TOOLS_BIN}/general_setup --usage
 }
 
 #
@@ -267,14 +270,6 @@ usage()
 report_usage=0
 found=0
 for arg in "$@"; do
-	if [ $found -eq 1 ]; then
-		tools_git=$arg
-		break;
-	fi
-	if [[ $arg == "--tools_git" ]]; then
-		found=1
-	fi
-
 	#
 	# We do the usage check here, as we do not want to be calling
 	# the common parsers then checking for usage here.  Doing so will
@@ -287,14 +282,47 @@ done
 
 #
 # Check to see if the test tools directory exists.  If it does, we do not need to
-# clone the repo.
+# clone the repo. 
 #
-if [ ! -d "test_tools" ]; then
-        git clone $tools_git test_tools
-        if [ $? -ne 0 ]; then
-                exit_out "pulling git $tools_git failed." 1
+# Note: this is the "standard way" as of March 2026
+#
+attempt_tools_wget()
+{
+        if [[ ! -d "$TOOLS_BIN" ]]; then
+                wget ${tools_git}/archive/refs/heads/main.zip
+                if [[ $? -eq 0 ]]; then
+                        unzip -q main.zip
+                        mv test_tools-wrappers-main ${TOOLS_BIN}
+                        rm main.zip
+                fi
         fi
-fi
+}
+attempt_tools_curl()
+{
+        if [[ ! -d "$TOOLS_BIN" ]]; then
+                curl -L -O ${tools_git}/archive/refs/heads/main.zip
+                if [[ $? -eq 0 ]]; then
+                        unzip -q main.zip
+                        mv test_tools-wrappers-main ${TOOLS_BIN}
+                        rm main.zip
+                fi
+        fi
+}
+attempt_tools_git()
+{
+        if [[ ! -d "$TOOLS_BIN" ]]; then
+                git clone $tools_git "$TOOLS_BIN"
+                if [ $? -ne 0 ]; then
+                        exit_out "Error: pulling git $tools_git failed." 101
+                fi
+        fi
+}
+
+attempt_tools_wget
+attempt_tools_curl
+attempt_tools_git
+
+# End of test tools installation
 
 if [ $report_usage -eq 1 ]; then
 	usage $0
@@ -312,7 +340,7 @@ fi
 # to_tuned_setting: tuned setting
 #
 
-source test_tools/general_setup "$@"
+source ${TOOLS_BIN}/general_setup "$@"
 
 # Install needed packages based on what's listed in the wrapper's json file
 ${TOOLS_BIN}/package_tool --no_packages $to_no_pkg_install --wrapper_config ${run_dir}/iozone-wrapper.json
@@ -1813,7 +1841,7 @@ else
 fi
 
 cp ${curdir}/meta_data.yml $results_dir
-${curdir}/test_tools/move_data $curdir ${results_dir}
+${TOOLS_BIN}/move_data $curdir ${results_dir}
 if [[ $auto -eq 0 ]]; then
 	reduce_non_auto_data $iozone_output_file > $out_dir/iozone_summary
 	cp -R ${results_dir} $out_dir
@@ -1840,7 +1868,7 @@ mv /tmp/results_iozone.csv ${results_dir}
 pushd ${results_dir} > /dev/null
 tar cf /tmp/results_iozone_${to_tuned_setting}.tar *
 popd > /dev/null
-${curdir}/test_tools/save_results --curdir $curdir --home_root $to_home_root --tar_file "/tmp/results_iozone_${to_tuned_setting}.tar" --test_name ${test_name} --tuned_setting=$to_tuned_setting --version None --user $to_user
+${TOOLS_BIN}/save_results --curdir $curdir --home_root $to_home_root --tar_file "/tmp/results_iozone_${to_tuned_setting}.tar" --test_name ${test_name} --tuned_setting=$to_tuned_setting --version None --user $to_user
 popd >& /dev/null
 
 # Shutdown PCP and clean up after ourselves
